3eea339538fa3425a0793e5053d462d5053ce162
naoi0522 <74036170+naoi0522@users.noreply.github.com>
2020-11-16 12:27:38 +0900

    user.pyのメソッド追加

--- quizapp/models/user.py
+++ quizapp/models/user.py
@@ -8,3 +8,27 @@ class User(db.Model):
 
     userID = db.Column(db.String(20), primary_key=True)
     password = db.Column(db.String(20))
+
+    def get_user_one(self, userID):
+        user = db.session.query(User).get(userID)
+
+        return user
+
+    def get_user_all(self):
+        user_list = db.session.query(User).all()
+
+        return user_list
+
+    def user_count(self):
+        user_count = db.session.query(User).count()
+
+        return user_count
+
+    def register_user(self, userID, password):
+        record = User(
+            userID=userID,
+            password=password
+        )
+
+        db.session.add(record)
+        db.session.commit()
