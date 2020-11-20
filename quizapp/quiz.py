78296c66c03eae2afc058df67f2ebbe4c3739597
naoi0522 <74036170+naoi0522@users.noreply.github.com>
2020-11-20 11:59:37 +0900

    update_quizメソッドの追加+@
    + get_quiz_from_userID()
    + trim_spaces()

--- quizapp/quiz.py
+++ quizapp/quiz.py
@@ -59,3 +59,21 @@ def registerquiz():
         return render_template('quiz/registerquiz.html',
                                title="クイズ登録", current_userID=session['userID'], login=session['login'],
                                is_post=False)
+
+
+@bp.route('/update', methods=['GET', 'POST'])
+def registerquiz():
+    if request.method == "POST":
+        quizID = int(request.form['quizID'])
+        problem = request.form['problem']
+        correct = int(request.form['correct'])
+
+        correct = qmng.update_quiz(quizID, problem, correct)
+
+        return render_template('quiz/update.html',
+                               title="クイズ更新", current_userID=session['userID'], login=session['login'],
+                               is_post=True, correct=correct)
+    else:
+        return render_template('quiz/update.html',
+                               title="クイズ更新", current_userID=session['userID'], login=session['login'],
+                               is_post=False)
