751c1bb8cf7f3269a7893398919a330e1411863c
naoi0522 <74036170+naoi0522@users.noreply.github.com>
2020-11-24 15:01:39 +0900

    __init__.py json対応化

--- quizapp/__init__.py
+++ quizapp/__init__.py
@@ -1,25 +1,33 @@
 from flask import *
 from quizapp.database import init_db
 import quizapp.models
-from quizapp.quizmanage import *
 import quizapp.config
+from quizapp.buildjson import *
 
 #app = Flask(__name__)
+
+bjson = BuildJSON()
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('quizapp.config.Config')
+    app.config["JSON_AS_ASCII"] = False
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
     init_db(app)
 
-    @app.route('/', methods=['GET', 'POST'])
+    @app.route('/', methods=['GET'])
     def index():
         if not 'userID' in session:
             session['userID'] = None
         if not 'login' in session:
             session['login'] = False
+
+        status = bjson.build_status(session['userID'], session['login'])
+
+        # return jsonify(status)
+
         return render_template('index.html',
                                title="メインページ", current_userID=session['userID'], login=session['login'])
 
