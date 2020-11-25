11c018529db0767b9ddee757f6389fb3020f4bfc
naoi0522 <74036170+naoi0522@users.noreply.github.com>
2020-11-25 11:23:53 +0900

    auth.py json対応化

--- quizapp/auth.py
+++ quizapp/auth.py
@@ -1,5 +1,6 @@
 #import functools
 from quizapp.usermanage import *
+from quizapp.buildjson import *
 from flask import(
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
@@ -8,6 +9,7 @@ from flask import(
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 umng = UserManage()
+bjson = BuildJSON()
 
 
 @bp.route('/registeruser', methods=['GET', 'POST'])
@@ -16,16 +18,29 @@ def register_user():
         userID = request.form['userID']
         passwd = request.form['passwd']
 
-        session['login'] = umng.register_user(userID, passwd)
+        CRUD_correct = umng.register_user(userID, passwd)
 
-        if session['login']:
+        if CRUD_correct:
             session['userID'] = userID
+            session['login'] = CRUD_correct
         else:
             session['userID'] = None
+            session['login'] = CRUD_correct
+
+        login_status = bjson.build_login_status(
+            session['userID'], session['login'])
+        status = bjson.build_status(request.method, "")
+        user = bjson.build_user(userID, "")
+
+        # return jsonify(login_status, status, user)
 
         return render_template('auth/registeruser.html',
                                title="ユーザ登録", current_userID=session['userID'], login=session['login'], is_post=True, userID=userID)
     else:
+        status = bjson.build_status(request.method, "")
+
+        # return jsonify(status)
+
         return render_template('auth/registeruser.html',
                                title="ユーザ登録", current_userID=session['userID'], login=session['login'], is_post=False)
 
@@ -36,23 +51,41 @@ def login():
         userID = request.form['userID']
         passwd = request.form['passwd']
 
-        session['login'] = umng.authenticate_user(userID, passwd)
+        CRUD_correct = umng.authenticate_user(userID, passwd)
 
-        if session['login']:
+        if CRUD_correct:
             session['userID'] = userID
+            session['login'] = CRUD_correct
         else:
             session['userID'] = None
+            session['login'] = CRUD_correct
+
+        login_status = bjson.build_login_status(
+            session['userID'], session['login'])
+        status = bjson.build_status(request.method, "")
+        user = bjson.build_user(userID, "")
+
+        # return jsonify(login_status, status, user)
 
         return render_template('auth/login.html',
                                title="ログイン", current_userID=session['userID'], login=session['login'], is_post=True, userID=userID)
     else:
+        status = bjson.build_status(request.method, "")
+
+        # return jsonify(status)
+
         return render_template('auth/login.html',
                                title="ログイン", current_userID=session['userID'], login=session['login'], is_post=False)
 
 
-@bp.route('/logout', methods=['GET', 'POST'])
+@bp.route('/logout', methods=['GET'])
 def logout():
     session.pop('userID', None)
     session['login'] = False
+
+    login_status = bjson.build_login_status(
+        session['userID'], session['login'])
+
+    # return jsonify(login_status)
 
     return redirect(url_for('index'))
